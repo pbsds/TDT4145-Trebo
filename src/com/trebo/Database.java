0ca27973ecde3d2c1d197fc63aeb44b48ba3838e
Peder Bergebakken Sundt <pbsds@hotmail.com>
2017-03-06 13:46:15 +0100

    ech

--- src/com/trebo/Database.java
+++ src/com/trebo/Database.java
@@ -1,36 +1,57 @@
 package com.trebo;
 import java.sql.*;
 
-
 public class Database {
     private String address, database, username, password;
+    Connection con = null;
 
     public Database(String address, String database, String username, String password){
         this.address = address;
         this.username = username;
         this.database = database;
         this.password = password;
-    }
-    public void template_update() throws SQLException{
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
         try {
             con = DriverManager.getConnection(this.address, this.username, this.password);
+        }catch(SQLException e){
+            System.out.println("Couldn't connect to server: " + this.address);
+            System.out.println(e);
+            System.exit(1);
+        }
+    }
 
-            pstmt = con.prepareStatement(
-                    "UPDATE EMPLOYEES " +
-                            "SET CAR_NUMBER = ? " +
-                            "WHERE EMPLOYEE_NUMBER = ?");
+    public void template_update() throws SQLException{//INSERT, UPDATE or DELETE
+        PreparedStatement pstmt = this.con.prepareStatement(
+                "UPDATE EMPLOYEES " +
+                        "SET CAR_NUMBER = ? " +
+                        "WHERE EMPLOYEE_NUMBER = ?");
 
-            pstmt.setInt(1, 33);
-            pstmt.setInt(2, 44);
+        pstmt.setInt(1, 33);
+        pstmt.setInt(2, 44);
 
-            //pstmt.executeUpdate();
+        try {
+            pstmt.executeUpdate();
         }
         finally {
             if (pstmt != null) pstmt.close();
         }
     }
+    public void template_select() throws SQLException{
+        PreparedStatement pstmt = this.con.prepareStatement(
+                "UPDATE EMPLOYEES " +
+                        "SET CAR_NUMBER = ? " +
+                        "WHERE EMPLOYEE_NUMBER = ?");
 
+        pstmt.setInt(1, 1337);
+        pstmt.setInt(2, 42);
+
+        ResultSet out;
+        try {
+            out = pstmt.executeQuery();
+        }
+        finally {
+            pstmt.close();
+        }
+
+        //out
+    }
 }
--- src/com/trebo/trebo.java
+++ src/com/trebo/trebo.java
@@ -4,16 +4,19 @@ import java.sql.SQLException;
 public class trebo {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
         Database db = new Database("jdbc:mysql://mysql.stud.ntnu.no:3306/pederbs_trebodb",
                 "pederbs_trebodb",
                 "pederbs_trebo",
                 "spismeg");
 
-
+        /*
         try {
-            db.template();
+            db.template_update();
         } catch (SQLException e) {
             //lolno
         }
+        */
     }
+
 }
